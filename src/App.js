f1ba5777288170010e0f00b9a52b1110b1a6667e
amarilisdev <amarilismoldes.dev@gmail.com>
2025-06-03 12:59:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [name, setName] = useState("");
+
+  const changeName = (event) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h2> {name ? `Hello, ${name}` : "Please insert your name"}</h2>
+      <input
+        type="text"
+        placeholder="Your name"
+        value={name}
+        onChange={changeName}
+        un
+      />
+    </div>
+  );
+}
